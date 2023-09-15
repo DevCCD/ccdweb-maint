3932a7bc9a634e6d23544880b0528bb7e0a40a93
DevCCD <ccdcompetitivdad@gmail.com>
2023-09-14 19:03:26 -0500

    14.09.2023

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+      <div class="left">
+          <h1>:(</h1>
+          <h2>¡LAMENTAMOS LAS MOLESTIAS!</h2>
+          <p>Este espacio se encuentra en mantenimiento.</p>
+          <p>Muy pronto estará disponible.</p>
+      </div>
+      <div class="right">
+          <img src="./background.png" alt="Imagen Mantenimiento CCD"/>
+      </div>
+  </div>
+  );
+}
+
+export default App;
